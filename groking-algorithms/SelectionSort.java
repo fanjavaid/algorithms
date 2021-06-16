efcea059090522b80569137a96897ce9f06f98ad
Fandi Akhmad <fanjavaid@gmail.com>
2021-06-16 16:40:32 +0700

    Add new learning: Selection Sort

---
+++ groking-algorithms/SelectionSort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class SelectionSort {
+	public static void main(String[] args) {
+		int[] data  = { 89, -9, 56, 7, 0 };
+		System.out.println(Arrays.toString(data));
+
+		int n = data.length;
+		for (int i = 0; i < n-1; i++) {
+			int min = i;
+			for (int j = i+1; j < n; j++) {
+				if (data[j] < data[min]) min = j;
+			}
+
+			int temp = data[min];
+			data[min] = data[i];
+			data[i] = temp;
+		}
+
+		System.out.println("Sorted to:");
+		System.out.println(Arrays.toString(data));
+	}
+}
