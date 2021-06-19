6971d03a6d9b187aa327e6ad984b4761bfc75ab6
Fandi Akhmad <fanjavaid@gmail.com>
2021-06-19 23:17:42 +0700

    Add learning : Recursion

---
+++ groking-algorithms/RecursionBinarySearch.java
@@ -0,0 +1,28 @@
+public class RecursionBinarySearch {
+	public static void main(String[] args) {
+		int[] arr = { -1, 10, 18, 75, 99, 1000 };
+		int x = -18;
+		System.out.println(x+" at index " + binarySearch(arr, x, 0, arr.length-1));
+	}
+	
+	public static int binarySearch(int[] arr, int x, int start, int end) {
+		int low = start;
+		int high = end;
+		int mid = (low+high)/2;
+
+		// Base case when the x is not found
+		if (low > high) return -1;
+
+		// Base case when the x is found
+		if (arr[mid] == x) {
+			return mid;
+		} else if (arr[mid] > x) {
+			high = mid-1;
+		} else {
+			low = mid+1;
+		}
+		
+		// Recursive case
+		return binarySearch(arr, x, low, high);
+	}
+}
---
+++ groking-algorithms/RecursionFactorial.java
@@ -0,0 +1,20 @@
+public class RecursionFactorial {
+	public static void main(String[] args) {
+		System.out.println(factorial(5));
+	}
+
+	public static int factorial(int n) {
+		// Base case
+		int returnValue = 0;
+
+		if (n == 1) {
+			return returnValue = 1;
+		}
+
+		returnValue = n * factorial(n-1);
+
+		System.out.println("factorial("+n+") return "+returnValue);
+
+		return returnValue;
+	}
+}
